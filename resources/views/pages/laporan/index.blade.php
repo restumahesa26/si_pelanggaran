fd72d9d08f3c8c4ebbedb280da6c2030dbe52b16
Mufti Restu Mahesa <mufti.restumahesa@gmail.com>
2023-06-13 22:17:09 +0700

    update

--- resources/views/pages/laporan/index.blade.php
+++ resources/views/pages/laporan/index.blade.php
@@ -10,40 +10,42 @@
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h5 class="card-title fw-semibold">10 Pelanggaran Siswa Terakhir</h5>
                     <div class="d-flex justify-content-start">
-                        <a href="{{ route('laporan.cetak') }}" class="btn btn-info me-2" target="_blank">Cetak Laporan</a>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <a href="{{ route('laporan.cetak') }}" class="btn btn-info" target="_blank">Cetak Laporan</a>
+                        @if (Auth::user()->role == 'Admin')
+                        <button type="button" class="btn btn-primary ms-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             Cetak Berdasarkan Kelas
                         </button>
                         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Wali Kelas</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <form action="{{ route('laporan.cetak-2') }}" method="GET" target="_blank">
-                                <div class="modal-body">
-                                    <div class="form-group">
-                                        <label for="kelas">Pilih Kelas</label><br>
-                                        <select class="kelas w-100" id="kelas" name="kelas" required>
-                                            <option value=""></option>
-                                            @foreach ($kelas as $data)
-                                            <option value="{{ $data->id }}" @if(old('id') == $data->id) selected @endif>{{ $data->jenjang }} {{ $data->kelas }}</option>
-                                            @endforeach
-                                        </select>
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Tambah Data Wali Kelas</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
                                     </div>
+                                    <form action="{{ route('laporan.cetak-2') }}" method="GET" target="_blank">
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="kelas">Pilih Kelas</label><br>
+                                                <select class="kelas w-100" id="kelas" name="kelas" required>
+                                                    <option value=""></option>
+                                                    @foreach ($kelas as $data)
+                                                    <option value="{{ $data->id }}" @if(old('id') == $data->id) selected @endif>{{ $data->jenjang }} {{ $data->kelas }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Tutup</button>
+                                            <button type="submit" class="btn btn-primary">Cetak</button>
+                                        </div>
+                                    </form>
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary"
-                                        data-bs-dismiss="modal">Tutup</button>
-                                    <button type="submit" class="btn btn-primary">Cetak</button>
-                                </div>
-                            </form>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                        @endif
                     </div>
                 </div>
                 <div class="table-responsive">
